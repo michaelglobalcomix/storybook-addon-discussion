0c1bebd1ffd8c20fa837661c1ac7027e51e8599c
Michael Camden <michael@globalcomix.com>
2024-01-26 14:27:59 -0800

    Update to latest storybook Add hack to resolve bug which doesn't allow the tab to become visible

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "./local-preset.js",
+    "@storybook/addon-mdx-gfm"
   ],
   framework: {
     name: "@storybook/react-vite",
--- src/components/TabContent.tsx
+++ src/components/TabContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import { CommentsList } from "@components/CommentsList";
 import { CommentBox } from "@components/CommentBox";
@@ -23,8 +23,39 @@ export const TabContent: React.FC<TabContentProps> = ({
     paramData
   );
 
+  // https://github.com/storybookjs/storybook/issues/25322
+  // There is an issue in latest storybook which will render a tab, but not make
+  // it visible this hack will make it visible by walking up the DOM tree until
+  // it finds a specific div and set it to be visible
+
+  const isHiddenContainerDiv = (node: HTMLElement) => {
+    if (node.tagName === "DIV" && node.hasAttribute("hidden")) {
+      return true;
+    }
+    return false;
+  };
+
+  const shouldStop = (node: HTMLElement) => {
+    return node.hasAttribute("role") && node.getAttribute("role") === "main";
+  };
+
+  const container = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    setTimeout(() => {
+      let node: HTMLDivElement | HTMLElement = container.current;
+      while (!shouldStop(node)) {
+        if (isHiddenContainerDiv(node)) {
+          node.removeAttribute("hidden");
+          break;
+        }
+
+        node = node.parentElement;
+      }
+    }, 10);
+  }, []);
+
   return (
-    <TabWrapper>
+    <TabWrapper ref={container}>
       <TabInner>
         <CommentsList isLoading={isLoading} error={error} comments={comments} />
         <br />
